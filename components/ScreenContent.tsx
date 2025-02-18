dd8d99c28633d919123720c88e4887c354334d87
NyLaurent <laulanyumbayire@gmail.com>
2025-02-18 06:57:19 -0600

    Initial commit

---
+++ components/ScreenContent.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const ScreenContent = () => {
+  return (
+    <SafeAreaView>
+      <Text>ScreenContent</Text>
+    </SafeAreaView>
+  )
+}
+
+export default ScreenContent
